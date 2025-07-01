6a15f100490184419737f44a2018617285fb3047
Bruno Seixas <102114046+iBrunos@users.noreply.github.com>
2025-07-01 09:08:50 -0300

    .

--- api/dist/main.js
+++ api/dist/main.js
@@ -6,7 +6,7 @@ const common_1 = require("@nestjs/common");
 const bodyParser = require("body-parser");
 async function bootstrap() {
     try {
-        const port = 3001;
+        const port = 8080;
         const app = await core_1.NestFactory.create(app_module_1.AppModule);
         app.use(bodyParser.json({ limit: '10mb' }));
         app.useGlobalPipes(new common_1.ValidationPipe());
